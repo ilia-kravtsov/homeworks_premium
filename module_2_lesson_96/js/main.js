cc2ab2c1948745553dc5aca8d3e36994b150bd97
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2024-02-09 22:09:31 +0400

    module_2_lesson_110_has completed

--- module_2_lesson_96/js/main.js
+++ module_2_lesson_96/js/main.js
@@ -11,5 +11,46 @@ document.addEventListener('click', (e) => {
   } else {
     document.body.classList.add('page--opened-menu')
   }
-  
 })
+
+// ______________________________ 109 modal opened homework ____________________________
+
+function modalOpener(e) {
+  e.preventDefault()
+  document.body.classList.toggle('page--opened-modal')
+}
+// ______________________________ the first scenario____________________________
+
+document.addEventListener('click', (e) => {
+
+  const target = e.target
+
+  const modalCLoseIcon = target.closest('.modal__close-button')
+  const ideaImageLinkButton = target.closest('.idea__image-link-button')
+
+  if (ideaImageLinkButton || modalCLoseIcon || target.classList.contains('modal')) modalOpener(e)
+})
+
+// ______________________________ the second scenario ____________________________
+
+// const modalCLoseIcon = document.querySelector('.modal__close-button')
+// const ideaImageLinkButton = document.querySelector('.idea__image-link-button')
+//
+// modalCLoseIcon.addEventListener('click',(e) => {
+//   modalOpener(e)
+// })
+// ideaImageLinkButton.addEventListener('click',(e) => {
+//   modalOpener(e)
+// })
+//
+// document.addEventListener('click', (e) => {
+//   e.target.classList.contains('modal') && modalOpener(e)
+// })
+
+// ______________________________ or the third one ____________________________
+// document.addEventListener('click', (e) => {
+//   const target = e.target;
+//   const modalCLoseIcon = target.closest('.modal__close-button')
+//
+//   if (target.matches('.idea__image-link-button') || modalCLoseIcon || target.classList.contains('modal')) modalOpener(e);
+// });
